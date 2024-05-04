6d59696bb7c57d4778ecce0cccd65fadf0793103
joaovpinheiros <joaovpinheirodev@gmail.com>
2024-05-03 23:06:58 -0300

    docs: Novo objeto e seus atributos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Primeiro_Projeto_Em_DotNet;
+using Primeiro_Projeto_Em_DotNet.models;
+
+// Novo Objeto Pessoa
+Person person = new Person("Joao");
+
+// Determinar Idade
+person.Age = 18;
+
+// Executar metodo de apresentação
+person.Presentation();
+
+
---
+++ models/Pessoa.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primeiro_Projeto_Em_DotNet.models
+{
+    public class Person
+    {
+        // Atributos
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double height { get; set; }
+
+        // Constructor Item obrigario na criação do objeto
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        // Metodos
+
+        public void Presentation()
+        {
+            PrintDisplay($"Hi, my name is {Name},  i´m {Age} years old");
+        }
+
+        public void PrintDisplay(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
