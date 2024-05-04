4d72a714a5a100814338d2d5f2a434d5326a966c
joaovpinheiros <joaovpinheirodev@gmail.com>
2024-05-04 17:20:08 -0300

    atualizando arquivos

--- obj/Debug/net8.0/Primeiro-Projeto-Em-DotNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Primeiro-Projeto-Em-DotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primeiro-Projeto-Em-DotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e218a34a266a817f04b159d0738e5a2d012f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e5bf7ccb85d5b99d250d86c11c4ac3a8e662373")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primeiro-Projeto-Em-DotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primeiro-Projeto-Em-DotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
